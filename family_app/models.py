b62fd7d64ee73e147d4f9da696c9b9a25514099e
rohimexc <rohimexcellent5918@gmail.com>
2023-02-20 20:08:13 +0800

    20/02/2023

--- family_app/models.py
+++ family_app/models.py
@@ -1,7 +1,6 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 
 class OptionCity(models.Model):
--- family_project/urls.py
+++ family_project/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
-
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('family_app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
 ]
