0158730015cb00f8c2fdb3bdc1ca652186af7950
rohimexc <rohimexcellent5918@gmail.com>
2023-02-20 22:49:05 +0800

    20/02/2023

--- family_project/urls.py
+++ family_project/urls.py
@@ -17,11 +17,15 @@ from django import views
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('family_app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
 if settings.DEBUG:
